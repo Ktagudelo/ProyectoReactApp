e1f7952902390d3feeadc6eb8800f685b23f9bbb
ktagudelo <ospinacatalina2@gmail.com>
2020-08-05 22:46:01 -0500

    Updates

---
+++ precache-manifest.d152c98ea89cd3fea595dd2020686691.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061229d45b56982ec10b42e499336f95",
+    "url": "/ProyectoReactApp/index.html"
+  },
+  {
+    "revision": "24cb56c816c63409da64",
+    "url": "/ProyectoReactApp/static/css/main.dbbd422e.chunk.css"
+  },
+  {
+    "revision": "6980d8acbbbf8f1f86ab",
+    "url": "/ProyectoReactApp/static/js/2.67a0641a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ProyectoReactApp/static/js/2.67a0641a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24cb56c816c63409da64",
+    "url": "/ProyectoReactApp/static/js/main.1f52e3f8.chunk.js"
+  },
+  {
+    "revision": "06fbf46b0ae4073b7bd3",
+    "url": "/ProyectoReactApp/static/js/runtime-main.71087673.js"
+  },
+  {
+    "revision": "c42e4886f21aa656d90546f4bdbda4ed",
+    "url": "/ProyectoReactApp/static/media/Design.c42e4886.jpg"
+  }
+]);
